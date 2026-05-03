2d37e56bcbae088eb835c61e872bb7fd01060108
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-05-03 14:44:10 +0300

    refactor: reformat core.rs

--- src/shell/core.rs
+++ src/shell/core.rs
@@ -1,16 +1,15 @@
-use std::io::Error;
-
 pub trait ShellInterpreter<T> {
-    fn run<R: ShellCommandProvider<T>>(tokens: &[T]) -> Result<Vec<u8>, Error>;
+    fn run<R: ShellCommandProvider<T>>(tokens: &[T]) -> Result<Vec<u8>, std::io::Error>;
 }
 
 pub trait ShellTokenizer<T> {
-    fn tokenize(input: &str) -> Result<Vec<T>, Error>;
+    fn tokenize(input: &str) -> Result<Vec<T>, std::io::Error>;
 }
 
 pub trait ShellCommandProvider<T> {
     fn run(cmd: &str, tokens: &[T]) -> Result<String, std::io::Error>;
     fn get_commands() -> Vec<&'static str>;
+    fn search_command(query: &str) -> Result<Vec<String>, std::io::Error>;
 }
 
 pub trait ShellCommand<T> {
