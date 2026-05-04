e0b5f61cc1531328a2f5b192251b193a54e479cd
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-05-04 13:59:00 +0300

    feat: update auto completion to handle tab clicks properly

--- src/modules/event_handler/key_handler/tab.rs
+++ src/modules/event_handler/key_handler/tab.rs
@@ -9,7 +9,7 @@ use crate::shell::{
     core::{ShellCommandProvider, ShellInterpreter, ShellTokenizer},
     Shell,
 };
-use std::io::Error;
+use std::{collections::HashSet, io::Error};
 
 impl Shell {
     pub(crate) fn handle_tab<
@@ -24,7 +24,7 @@ impl Shell {
             self.tab_query = self.buffer.trim().to_string();
         }
 
-        let mut search_result = vec![];
+        let mut search_result = HashSet::new();
 
         let builtin_commands = CommandProvider::get_commands();
         let found_builtin_command = builtin_commands
@@ -32,7 +32,7 @@ impl Shell {
             .find(|c| c.starts_with(&self.tab_query));
 
         if found_builtin_command.is_some() {
-            search_result.push(found_builtin_command.unwrap().to_string());
+            search_result.insert(found_builtin_command.unwrap().to_string());
         }
 
         search_result.extend(CommandProvider::search_command(&self.tab_query)?);
@@ -41,12 +41,8 @@ impl Shell {
             return Ok(());
         }
 
-        let key_option = search_result.get(self.tab_index as usize);
-
-        if key_option.is_some() {
-            let key = key_option.unwrap();
-
-            self.buffer = key.to_string() + " ";
+        if search_result.len() == 1 {
+            self.buffer = search_result.iter().nth(0).unwrap().to_string() + " ";
 
             execute!(
                 self.stdout,
@@ -56,11 +52,31 @@ impl Shell {
                 Print(&self.buffer),
                 MoveToColumn((&self.buffer.len() + 2) as u16)
             )?;
-            if self.tab_index < search_result.len() as u8 {
-                self.tab_index += 1;
-            }
+
+            return Ok(());
+        } else if self.tab_index >= 1 {
+            let results = search_result
+                .iter()
+                .fold(String::new(), |acc, s| acc + " " + s);
+
+            dbg!(&results);
+
+            execute!(
+                self.stdout,
+                Clear(ClearType::CurrentLine),
+                MoveToColumn(0),
+                Print(super::PREFIX),
+                Print(&self.buffer),
+                Print("\r\n"),
+                Print(results.trim()),
+                Print("\r\n"),
+                Print(super::PREFIX),
+                Print(&self.buffer),
+                MoveToColumn((&self.buffer.len() + 2) as u16),
+            )?;
         }
 
+        self.tab_index += 1;
         Ok(())
     }
 }
