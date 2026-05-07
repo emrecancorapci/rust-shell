0779ad0935d7c527bcf6bc3d9bef2f21cfaff6e8
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-05-08 02:07:32 +0300

    fix: update builtin exit command

--- src/modules/provider/builtin/exit.rs
+++ src/modules/provider/builtin/exit.rs
@@ -1,12 +1,14 @@
 use std::io::{Error, ErrorKind};
 
-use crate::{shell::core::ShellCommand, modules::tokenizer::Token};
+use crate::{modules::tokenizer::Token, shell::core::ShellCommand};
 
 pub struct Exit {}
 
 impl ShellCommand<Token> for Exit {
     fn run(tokens: &[Token]) -> Result<String, Error> {
-        if tokens.get(2) == Some(&Token::Value("0".to_string())) {
+        if tokens.len() > 2 && tokens.get(2) == Some(&Token::Value("0".to_string())) {
+            return Err(Error::new(ErrorKind::Interrupted, ""));
+        } else if tokens.len() == 1 {
             return Err(Error::new(ErrorKind::Interrupted, ""));
         } else {
             return Err(Error::new(
